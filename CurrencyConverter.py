91c8ca222e72f9d826cd3074c64d0e86e9094b7d
hirahokad <hirahokad@gmail.com>
2021-06-16 12:13:21 -0700

    Fixed bugs with the conversion - Also added a rounding modifier to look clearner
    added exit checks for when the user types "stop"
    
    Fixed output to show post and pre conversion

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,57 +1,67 @@
 import time
 
 eur = 0.83
-jpy = 0.0091
-cny = 0.16
-mxn = 0.050
+jpy = 110.478
+cny = 6.40
+mxn = 20.26
 usd = 1
 start_Currency = "usd"
 currencyCheck = ["usd","mxn","cny","jpy","eur"]
+start_Amount = "usd"
+converted = 0.00
+tracker = 0
 
 status = True
 
 def converter(currency):
+    global converted
     status = True
+    if ( currency == "stop" ) :
+        exit()
+
     if ( currency == "eur"):
-        converted = start_Converstion * eur
+        converted = round(start_Amount * eur,2)
         status = False
     elif ( currency == "jpy"):
-        converted = start_Converstion * jpy
+        converted = round(start_Amount * jpy,2)
         status = False
     elif ( currency == "cny"):
-        converted = start_Converstion * cny
+        converted = round(start_Amount * cny,2)
         status = False
     elif ( currency == "mxn"):
-        converted = start_Converstion * mxn
+        converted = round(start_Amount * mxn,2)
         status = False
     elif ( currency == "usd"):
-        converted = start_Converstion * usd
+        converted = round(start_Amount * usd,2)
         status = False
     else:
         print("That's not a valid option")
 
 
-print("Welcome to my Currency Converter! Follow the prompts and then please type 1 when you're done")
+print("Welcome to my Currency Converter! Follow the prompts and then please type stop when you're done")
 time.sleep(2)
 
 
 while status:
     if start_Currency in currencyCheck:
-        print("What is the currency you're using now?")
+        start_Currency = input("What is the currency you're using now?\n").lower()
+        if ( start_Currency == "stop" ):
+            exit()
+
+        start_Amount = float(input("Enter the amount of money you wish to convert:\n"))
+        #runs converter function to convert current currency
         converter(start_Currency)
+        #If option is valid we want to break
         status = False
     else:
         print("Please enter a Valid option.")
+        #If Option isn't valid we want to loop
+        status = True
 
 status = True
 time.sleep(1)
 
-start_Amount = float(input("Enter the amount of money you wish to convert:\n"))
-start_Converstion = start_Currency * start_Amount
-
-requested_Currency = input("Which currency do you wish to convert to?(USD, EUR, JPY, CNY, MXN)\n").lower()
-
-while status:
+while status and tracker == 0:
     if start_Currency in currencyCheck:
         requested_Currency = input("Which currency do you wish to convert to?(USD, EUR, JPY, CNY, MXN)\n").lower()
         converter(requested_Currency)
@@ -61,7 +71,5 @@ while status:
 
 converter(requested_Currency)
 
-print(f"You will have {converted} {requested_Currency}")
-
-
-
+print("Start : " + start_Currency + " : " + str(start_Amount))
+print("End : " + requested_Currency + " : " + str(converted)) 
